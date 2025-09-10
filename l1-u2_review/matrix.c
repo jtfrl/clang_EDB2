6000ea14471b18ec60769355a2810724e69592fe
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-09-10 17:30:57 +0000

    add still to be impl

--- l1-u2_review/matrix.c
+++ l1-u2_review/matrix.c
@@ -1,2 +1,13 @@
 /*q. 4 - Lista de Exerc ́ıcios - Revisão de C para EDB2 */
 #include <stdio.h>
+
+void readVal(int* mint[][]){}
+
+int suMatrix(int* mint[][]){}
+
+int main(){
+    int mInt[3][3]={};
+
+
+    return 0;
+}
--- l1-u2_review/vect.c
+++ l1-u2_review/vect.c
@@ -1,2 +1,30 @@
-/*q. 2 - Lista de Exerc ́ıcios - Revisão de C para EDB2 */
+/*q. 3 - Lista de Exercıcios - Revisão de C para EDB2 */
 #include <stdio.h>
+
+void readVal(int* vint[]){
+    int v=0;
+
+    for(int i=0; i<5;i++){
+         printf("Insira os valores do vetor na posição %d \n", (i+1));
+         scanf("%d", v);
+         vint[i]=v;
+    }
+
+    //return vint[];
+
+}
+
+int sumVect(int* vint[]){
+
+    int soma=0;
+
+    return soma;
+}
+
+int main(){
+    int vInt[5]={};
+
+
+
+    return 0;
+}
