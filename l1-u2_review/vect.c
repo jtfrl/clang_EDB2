ef6b435077f3cfd0b031d2fe5e1ed0f87b2b7ed8
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-09-11 03:06:34 +0000

    add works fine vect

--- l1-u2_review/vect.c
+++ l1-u2_review/vect.c
@@ -1,22 +1,23 @@
 /*q. 3 - Lista de Exercıcios - Revisão de C para EDB2 */
 #include <stdio.h>
 
-void readVal(int* vint[]){
-    int v=0;
+void readVal(int* v){
+    int n=0;
 
     for(int i=0; i<5;i++){
          printf("Insira os valores do vetor na posição %d \n", (i+1));
-         scanf("%d", v);
-         vint[i]=v;
+         scanf("%d", &n);
+         printf("\n");
+         v[i]=n;
     }
-
-    //return vint[];
-
 }
 
-int sumVect(int* vint[]){
-
+int sumVect(int vint[]){
     int soma=0;
+    for(int j=0; j<5; j++){
+        soma=soma+vint[j];
+    }
+    printf("Soma dos valores do vetor: %d \n", soma);
 
     return soma;
 }
@@ -24,7 +25,8 @@ int sumVect(int* vint[]){
 int main(){
     int vInt[5]={};
 
-
+    readVal(vInt);
+    sumVect(vInt);
 
     return 0;
 }
