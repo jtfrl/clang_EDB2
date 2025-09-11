875c65e85f2e5e51967ccf625c95e8ad176350a1
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-09-11 04:26:50 +0000

    add new struct

--- l1-u2_review/pessoa.c
+++ l1-u2_review/pessoa.c
@@ -2,12 +2,36 @@
 #include <stdio.h>
 
 struct Pessoa{
-    char nome;
+    char nome[50];
     int idade;
-    double altura;
+    float altura;
 };
 
+void insPessoa(struct Pessoa *p){
+    printf("Insira o nome: \n");
+    scanf("%s", p->nome);
+    // '->' acesso de membros de um ponteiro para um 'struct'
+
+    printf("Insira a idade: \n");
+    scanf("%d", &p->idade);
+
+    printf("Insira a altura: \n");
+    scanf("%f", &p->altura);
+
+}
+
+void showData(struct Pessoa *p){
+    printf("\n NOME: %s ", p->nome);
+    printf("\n NOME: %i ", p->idade);
+    printf("\n NOME: %.2f \n\n", p->altura);
+}
+
 int main(){
+    struct Pessoa p;
+
+    insPessoa(&p);
+    showData(&p);
+
 
     return 0;
 }
