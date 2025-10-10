ab61627f3917853e936e8cae999e911379134935
Ian Marques <iandaniel.vm@gmail.com>
2025-10-10 23:20:16 +0000

    Adicionar função para retornar o livro mais vendido
    Atualiza heap.c com uma função que retorna apenas o primeiro livro mais vendido da heap.

--- sistema_BIB/heap.c
+++ sistema_BIB/heap.c
@@ -121,8 +121,18 @@ void imprimirTopN(HeapMax *heap, int n) {
     }
 
     liberarHeap(temp);
+}
 
-    //PENDENTE: colocar função para mostrar só o 1o. mais vendido
+// Imprimir apenas o 1º livro mais vendido:
+void imprimirTop1(HeapMax *heap) {
+    if (heap->tamanho == 0) {
+        printf("Nenhum livro na heap!\n");
+        return;
+    }
+    Livro top = topoHeap(heap);
+    printf("\n📗 Livro mais vendido:\n");
+    printf("----------------------------------\n");
+    printf("%s (%d vendas)\n", top.titulo, top.vendas);
 }
 
 
