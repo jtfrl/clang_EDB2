a732bdcb8cc3f3aac99fc25e4ecc7d421edd6615
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-29 00:32:06 +0000

    add still impl show up trie and read txt file

--- trie/trie.c
+++ trie/trie.c
@@ -1,39 +1,47 @@
 #include "trie.h"
 
 No *criar_no(){
-  No *novo_no = (No *)malloc(sizeof(No));
+  No *novo_no = (No *)malloc(sizeof(No));  // reserva espaço na memória para  tipo No
   
-  if(novo_no)
-  {
-    novo_no->fim_da_palavra = false;
+  if(novo_no){
+    // o novo nó, se houver, recebe false
+    // para realizar a inserção da palavra
+    novo_no->fim_da_palavra = false; 
+
+    // cada um dos filhos do nó será null 
+    // (espaço reservados aos caracteres) que vão vir de 'inserir'
     for(int i = 0; i < ALFABETO; i++){
       novo_no->filhos[i] = NULL;
     }
   }
-  
   return novo_no;
 }
+
 
 void inserir(No *raiz, const char *palavra){
   No *atual = raiz;
   
   for(int i = 0; palavra[i] != '\0'; i++){
+    // cada letra na ASCII tem seu valor subtraido de 97
+    // isso agiliza a busca
     int indice = palavra[i] - 'a';
 
     if(atual->filhos[indice] == NULL)
       atual->filhos[indice] = criar_no();
 
-    atual = atual->filhos[indice];
+    atual = atual->filhos[indice]; 
+    // cada filho é atualizado com o caractere correspondente
+    // aqui é usado a string (vetor de caracteres)
   }
   
-  atual->fim_da_palavra = true;
+  atual->fim_da_palavra = true; // encerrado o laço, o fim da palavra é atingido
 }
 
 bool buscar(No *raiz, const char *palavra){
   No *atual = raiz;
   
   for(int i = 0; palavra[i] != '\0'; i++){
-    int indice = palavra[i] - 'a';
+    int indice = palavra[i] - 'a'; 
 
     if(atual->filhos[indice] == NULL)
       return false;
@@ -44,13 +52,12 @@ bool buscar(No *raiz, const char *palavra){
   return atual->fim_da_palavra;
 }
 
-//impl: função para mostrrar árvore inteira
-
 void liberar_trie(No *raiz){
   if(raiz == NULL)
     return;
 
   for(int i = 0; i < ALFABETO; i++)
+   // liberação de memória dinâmica (espaço reservado para o tipo 'No')
         liberar_trie(raiz->filhos[i]);
 
   free(raiz);
