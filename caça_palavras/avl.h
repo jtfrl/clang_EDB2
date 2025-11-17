245db49d77bcf731ccbb9430769c3742ab54d54c
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-11-17 02:32:51 +0000

    new impl with strcmp and free of memory use

--- caça_palavras/avl.h
+++ caça_palavras/avl.h
@@ -27,10 +27,14 @@ No* rot_esq_dir(No* x);
 No* inserir_p(No* no, char* p);
 No* remover_p(No* raiz, char* p);
 
-//No* menor_valorNo(No* no);
+No* menor_valorNo(No* no);
 
 void imprimir_em_ordem(No* raiz);
 
+
+// liberação de memória em no e na estrutura geral
+void liberar_no(No* no);
+void del_arv(No* raiz); // impede vazamentos de memória na AVL
 
 //###### FUNÇÕES DE PROCESSAMENTO DE ARQUIVO ######
 bool checkOpenFile(FILE* arqTxt);
